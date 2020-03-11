4c4d4a1458b8b7f76e0baca76b5f035a34585739
micredburn <micredburn@networkten.com.au>
2020-03-12 07:33:45 +1100

    Add new files

---
+++ src/components/focalContain.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FocalContain = ( props ) => {
+  return (
+    <div className='focalContainer'>
+      {props.children}
+    </div>
+  )
+}
+
+export default FocalContain;
